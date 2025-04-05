07aea327fd0bfd5778c1069b73d26d286a6bfda3
kabyarset <karialkarial6@gmail.com>
2025-04-05 03:44:52 -0400

    Create Map.tsx

---
+++ components/Map.tsx
@@ -0,0 +1,8 @@
+// components/Map.tsx
+export default function Map() {
+  return (
+    <div className="w-full h-full flex items-center justify-center text-white bg-slate-800">
+      <p>Map will appear here</p>
+    </div>
+  );
+}
