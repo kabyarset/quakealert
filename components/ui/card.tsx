553517a289cebb42f6418c895c60caa372f8d205
kabyarset <karialkarial6@gmail.com>
2025-04-05 23:45:30 -0400

    Delete components/ui directory

--- components/ui/card.tsx
+++
@@ -1,7 +0,0 @@
-// components/ui/card.tsx
-export function Card({ children, className }: any) {
-  return <div className={`rounded-lg shadow ${className}`}>{children}</div>;
-}
-export function CardContent({ children, className }: any) {
-  return <div className={className}>{children}</div>;
-}
